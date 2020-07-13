cf4916539c5de80a3433bb202da395d01563774b
TSevillaCS <tsevilla12@gmail.com>
2020-07-13 13:49:49 -0700

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,9 @@
+integer1 = raw_input("Enter first number")
+integer1 = int(integer1)
+
+integer2 = raw_input("Enter second number")
+integer2 = int(integer2)
+
+sum = integer1 + integer2
+
+print "sum is", sum
